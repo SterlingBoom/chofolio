ee1e82707f3f0c49048b2d1185a94685f8ff21a1
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-11-04 12:09:52 +0100

    edits

--- mongodb.ts
+++ mongodb.ts
@@ -1,11 +1,11 @@
 import { MongoClient } from "mongodb";
 const MONGODB_URI =
   "mongodb+srv://cermuel:chofolio12345678@cluster0.lwdzx8s.mongodb.net/chofolio?retryWrites=true&w=majority";
-if (!process.env.MONGO_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
-const uri: string = process.env.MONGO_URI;
+const uri: string = process.env.MONGODB_URI;
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
