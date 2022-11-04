b89f34a4ba38f3eaedce620a60448245ac311106
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-11-04 14:22:52 +0100

    Just work

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,34 +1,39 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import clientPromise from "../../../mongodb";
+// // import { NextApiRequest, NextApiResponse } from "next";
+// // import clientPromise from "../../../mongodb";
 
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   const client = await clientPromise;
-//   const db = client.db("chofolio");
+// // const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+// //   const client = await clientPromise;
+// //   const db = client.db("chofolio");
 
 //   if (req.method == "GET") {
-//     // const allPortfolios = await db.collection("chofolio").find({}).toArray();
-//     // res.json(allPortfolios);
+//     const allPortfolios = await db.collection("chofolio").find({}).toArray();
+//     res.json(allPortfolios);
 //     res.json({ message: "Hello, World!" });
 //   }
-//   if (req.method == "POST") {
-//     // const newPortfolio = req.body.newPortfolio;
-//     // let portfolio = await db.collection("chofolio").insertOne(newPortfolio);
-//     // res.json(portfolio);
-//     res.json({ message: "Hello, World!" });
-//   }
-// };
+// //   if (req.method == "POST") {
+// //     // const newPortfolio = req.body.newPortfolio;
+// //     // let portfolio = await db.collection("chofolio").insertOne(newPortfolio);
+// //     // res.json(portfolio);
+// //     res.json({ message: "Hello, World!" });
+// //   }
+// // };
 
-// export default handler;
+// // export default handler;
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "../../../mongodb";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "Ngene Samueel" });
+  const client = await clientPromise;
+  const db = client.db("chofolio");
+
+  const allPortfolios = await db.collection("chofolio").find({}).toArray();
+  res.status(200).json({ name: "API RUNNING" });
 }
