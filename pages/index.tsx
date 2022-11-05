01365d5b12549de12ceeb9f497568bf1c8bc2959
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-11-05 09:02:13 +0100

    Added head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
   const router = useRouter();
   return (
     <>
+      <Head>
+        <title>CHOFOLIO</title>
+        <meta name="description" content="Portfolio For Developers" />
+      </Head>
       <div>GO TO PORTFOLIO CREATION PAGE</div>
       <button
         onClick={() => {
