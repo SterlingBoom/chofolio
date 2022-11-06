0358f902506b501a991edfde464bdfc220e3e296
Ngene Samuel Obasi <samuelobasi2005@gmail.com>
2022-11-06 17:55:39 +0100

    Some stuffs

--- pages/portfolio/[unique_id].tsx
+++ pages/portfolio/[unique_id].tsx
@@ -17,6 +17,7 @@ const NewProfile = ({ data }: any) => {
   const [showdescription, setshowdescription] = useState<boolean>(false);
   const [reposUrl, setreposUrl] = useState<string>("");
   const [loading, setloading] = useState<boolean>(true);
+  const [pageAvailable, setpageAvailable] = useState(true);
   useEffect(() => {
     setuser(data);
     const getGithubRepoData = async () => {
@@ -45,6 +46,14 @@ const NewProfile = ({ data }: any) => {
     };
     getGithubData();
     getGithubRepoData();
+
+    const checkPage = async () => {
+      await data;
+      if (!data) {
+        setpageAvailable(false);
+      }
+    };
+    checkPage();
   }, [data]);
 
   const copy = (text: string) => navigator.clipboard.writeText(text);
@@ -53,111 +62,131 @@ const NewProfile = ({ data }: any) => {
     alert("Link copied to clipboard");
   };
 
-  if (loading === true) {
+  if (pageAvailable === false) {
     return (
-      <>
-        <div className={styles.loader}></div>
-      </>
+      <h1
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "100%",
+          textAlign: "center",
+          fontSize: "1.2em",
+        }}
+      >
+        Portfolio Not Available
+      </h1>
     );
-  }
-  if (loading === false) {
-    return (
-      <div className={styles.portfolio}>
-        <div className={styles.portfolioContainer}>
-          <div className={styles.basic}>
-            <img src={avatar} alt="Profile" />
+  } else if (pageAvailable === true) {
+    if (loading === true) {
+      return (
+        <>
+          <div className={styles.loader}></div>
+        </>
+      );
+    }
+    if (loading === false) {
+      return (
+        <div className={styles.portfolio}>
+          <div className={styles.portfolioContainer}>
+            <div className={styles.basic}>
+              <img src={avatar} alt="Profile" />
 
-            <div className={styles.extra}>
-              <div className={styles.name}>{user?.name}</div>
-              <div className={styles.username}>@{user?.username}</div>
-              <div className={styles.role}>{user?.role}</div>
+              <div className={styles.extra}>
+                <div className={styles.name}>{user?.name}</div>
+                <div className={styles.username}>@{user?.username}</div>
+                <div className={styles.role}>{user?.role}</div>
+              </div>
+            </div>
+
+            <div className={styles.header}>About Me</div>
+            <div className={styles.about}>{user?.about}</div>
+
+            <div className={styles.header}>Tech Stack</div>
+            <div className={styles.techStackContainer}>
+              {user?.skills.map((val, key: number) => {
+                return (
+                  <div className={styles.tech} key={key}>
+                    <div className={styles.techName}>{val.language}</div>
+                    <div className={styles.techLevel}>{val.level}</div>
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className={styles.header}>WORK Experience</div>
+
+            <div className={styles.workContainer}>
+              {user?.workXP?.map((val, key: number) => {
+                return (
+                  <div className={styles.work} key={key}>
+                    <div className={styles.title}>{val.title}</div>
+                    <div className={styles.company}>{val.company}</div>
+                    <div className={styles.date}>
+                      {val.start} - {val.end}
+                    </div>
+                    <button
+                      className={styles.handleDesc}
+                      onClick={() => setshowdescription(!showdescription)}
+                    >
+                      See {showdescription ? "less" : "more"}
+                    </button>
+                    {showdescription ? (
+                      <div className={styles.description}>
+                        {val.description}
+                      </div>
+                    ) : null}
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className={styles.repoHeader}>
+              <div>Repositories</div>{" "}
+              <a href={reposUrl} target="blank">
+                <BsBoxArrowUpRight />
+              </a>
+            </div>
+
+            <div className={styles.repoContainer}>
+              {repos?.slice(0, 6).map((val: Repo, key: number) => {
+                return (
+                  <div className={styles.repo} key={key}>
+                    <div className={styles.repoName}>
+                      <RiGitRepositoryLine />
+                      <a href={val.html_url}>{val.name}</a>
+                    </div>
+                    <div className={styles.repoDescription}>
+                      {val.description}
+                    </div>
+
+                    <div className={styles.language}>
+                      <div></div>
+                      {val.language}
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
 
-          <div className={styles.header}>About Me</div>
-          <div className={styles.about}>{user?.about}</div>
-
-          <div className={styles.header}>Tech Stack</div>
-          <div className={styles.techStackContainer}>
-            {user?.skills.map((val, key: number) => {
-              return (
-                <div className={styles.tech} key={key}>
-                  <div className={styles.techName}>{val.language}</div>
-                  <div className={styles.techLevel}>{val.level}</div>
-                </div>
-              );
-            })}
+          <div className={styles.footer}>
+            Copyright &copy; {new Date().getFullYear()} {user?.name}
           </div>
-
-          <div className={styles.header}>WORK Experience</div>
-
-          <div className={styles.workContainer}>
-            {user?.workXP?.map((val, key: number) => {
-              return (
-                <div className={styles.work} key={key}>
-                  <div className={styles.title}>{val.title}</div>
-                  <div className={styles.company}>{val.company}</div>
-                  <div className={styles.date}>
-                    {val.start} - {val.end}
-                  </div>
-                  <button
-                    className={styles.handleDesc}
-                    onClick={() => setshowdescription(!showdescription)}
-                  >
-                    See {showdescription ? "less" : "more"}
-                  </button>
-                  {showdescription ? (
-                    <div className={styles.description}>{val.description}</div>
-                  ) : null}
-                </div>
-              );
-            })}
-          </div>
-
-          <div className={styles.repoHeader}>
-            <div>Repositories</div>{" "}
-            <a href={reposUrl} target="blank">
-              <BsBoxArrowUpRight />
+          <button className={styles.copyLink} onClick={copyLink}>
+            Copy Portfolio Link
+          </button>
+          <div className={styles.cermuel}>
+            <a href="https://cermuel.vercel.app/" target="blank">
+              <span className={styles.one}>Want</span> a{" "}
+              <span className={styles.two}>Custom</span>{" "}
+              <span className={styles.three}>Template?</span>
             </a>
           </div>
-
-          <div className={styles.repoContainer}>
-            {repos?.slice(0, 6).map((val: Repo, key: number) => {
-              return (
-                <div className={styles.repo} key={key}>
-                  <div className={styles.repoName}>
-                    <RiGitRepositoryLine />
-                    <a href={val.html_url}>{val.name}</a>
-                  </div>
-                  <div className={styles.repoDescription}>
-                    {val.description}
-                  </div>
-
-                  <div className={styles.language}>
-                    <div></div>
-                    {val.language}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
         </div>
-
-        <div className={styles.footer}>
-          Copyright &copy; {new Date().getFullYear()} {user?.name}
-        </div>
-        <button className={styles.copyLink} onClick={copyLink}>
-          Copy Portfolio Link
-        </button>
-        <div className={styles.cermuel}>
-          <a href="https://cermuel.vercel.app/" target="blank">
-            <span className={styles.one}>Want</span> a{" "}
-            <span className={styles.two}>Custom</span>{" "}
-            <span className={styles.three}>Template?</span>
-          </a>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 };
 
